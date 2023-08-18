2a14c6e796ea8802fd2f658d15c3974685278f31
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2023-08-17 17:43:56 -0700

    testing if audio works in web version.

--- UI.py
+++ UI.py
@@ -4,3 +4,30 @@ from audio_recorder_streamlit import audio_recorder
 audio_bytes = audio_recorder()
 if audio_bytes:
     st.audio(audio_bytes, format="audio/wav")
+
+
+import base64
+
+import streamlit as st
+
+
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+
+
+if st.button("Test Speakers"):
+    autoplay_audio("ElevenLabs_2023-08-11T04_12_56.000Z_Julie_C2md8UcNeLKcOBWEB71e.mp3")
+    # Embed audio with autoplay
+    
+    
