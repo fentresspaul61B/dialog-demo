f5101b658aa43149ef5c840f67583a38b6a8878b
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2023-08-21 13:34:42 -0700

    removing un used imports in UI.

--- UI.py
+++ UI.py
@@ -1,5 +1,14 @@
+"""
+
+"""
+
+# Streamlit used to render web UI.
 import streamlit as st
+
+# Audio recorder used to record user audio during dialog.
 from audio_recorder_streamlit import audio_recorder
+
+# Base64 used to handle audio byte strings.
 import base64
 
 # Eleven labs is used for generating realistic sound speech.
@@ -7,22 +16,23 @@ from elevenlabs import generate, play, Voices
 from elevenlabs import set_api_key
 from elevenlabs.api import Voices
 
-# import librosa
-
-import json
+# Time is used to sleep, so that audio can finish playing, before st 
+# does a re run. 
 import time
 
-from pydub.utils import mediainfo
-
-
+# Audio helper functions used for the dialog.
 from st_helpers.audio_helpers import load_eleven_labs_voice
 from st_helpers.audio_helpers import autoplay_audio_from_bytes
 from st_helpers.audio_helpers import get_audio_duration
 from st_helpers.audio_helpers import generate_eleven_labs_audio 
 
 
+# Instantiating ElevenLabs voice.
 my_voice = load_eleven_labs_voice()
-# Audio recording button.
+
+
+# Audio recording button. Have to set key manually, so widgets do not 
+# conflict with eachother.
 audio_bytes = audio_recorder(key="123")
 
 
@@ -30,7 +40,6 @@ audio_bytes = audio_recorder(key="123")
 if not hasattr(st.session_state, 'processed'):
     st.session_state.processed = False
 
-# audio_bytes = audio_recorder()
 
 # Only process if the 'processed' flag is not set
 if audio_bytes and not st.session_state.processed:
@@ -39,17 +48,9 @@ if audio_bytes and not st.session_state.processed:
 
 
     audio = generate_eleven_labs_audio(response, my_voice)
-
-
-    # Send text to Eleven Labs API.
-    #audio = generate(
-     #   text=response,
-      #  voice=my_voice,
-       # model="eleven_monolingual_v1"
-    #)
     
-    end = time.time()
     st.write(end - start)
+    
     autoplay_audio_from_bytes(audio)
    
     # Convert audio bytes into .wav.
@@ -65,6 +66,7 @@ if audio_bytes and not st.session_state.processed:
 
     # Rerun the app
     st.experimental_rerun()
+
 
 # Reset audio_bytes and processed flag for the next interaction
 audio_bytes = None
