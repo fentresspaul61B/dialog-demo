4f5b764c810d3ac7bc614e32c98f28a1a25864af
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2023-08-21 11:05:21 -0700

    adding session state logic to try and overcome infinite loop.

--- UI.py
+++ UI.py
@@ -79,38 +79,42 @@ def autoplay_audio_from_bytes(audio_data: bytes):
 
 audio_bytes = audio_recorder()
 
-if audio_bytes:
-    # st.audio(audio_bytes, format="audio/wav")
-     
-    # autoplay_audio("ElevenLabs_2023-08-11T04_12_56.000Z_Julie_C2md8UcNeLKcOBWEB71e.wav")
 
+# Check if the session state has the 'processed' attribute
+if not hasattr(st.session_state, 'processed'):
+    st.session_state.processed = False
+
+audio_bytes = audio_recorder()
+
+# Only process if the 'processed' flag is not set
+if audio_bytes and not st.session_state.processed:
     start = time.time()
     response = "My response."
 
-    # 8. Send text to Eleven Labs API.
+    # Send text to Eleven Labs API.
     audio = generate(
         text=response,
         voice=my_voice,
         model="eleven_monolingual_v1"
     )
-        
-    # 9. Play the eleven labs audio.
-    # play(audio)
+    
     end = time.time()
     st.write(end - start)
-
-    # Embed audio with autoplay
-    # with open('myfile.wav', mode='wb') as f:
-    #     f.write(audio)
-
-    # autoplay_audio(LOCAL_AUDIO)
     autoplay_audio_from_bytes(audio)
-    
     time.sleep(3)
 
+    # Set the 'processed' flag
+    st.session_state.processed = True
+
+    # Rerun the app
     st.experimental_rerun()
 
+# Reset audio_bytes and processed flag for the next interaction
 audio_bytes = None
+st.session_state.processed = False
+
+
+
 
 def get_wav_duration(filename):
     with contextlib.closing(wave.open(filename,'r')) as f:
