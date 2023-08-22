df4579e41f4ba104a34a2b2072e167901d778b9f
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2023-08-22 11:12:09 -0700

    indent

--- UI.py
+++ UI.py
@@ -36,71 +36,72 @@ set_open_ai_token()
 # Instantiating ElevenLabs voice.
 my_voice = load_eleven_labs_voice()
 
-
-st.write("Press the button below to record audio.")
-# Audio recording button. Have to set key manually, so widgets do not 
-# conflict with eachother.
-audio_bytes = audio_recorder(
-        key="123", 
-        icon_name="square",
-        recording_color="#e8b62c",
-        neutral_color="#6aa36f",
-        text=""
-)
-
 user_input = st.text_input("Enter Password", type="password")
 
 if user_input == st.secrets["DEVA_USER_PW"]:
     st.write("PW correct")
-    
+ 
+
+
+    st.write("Press the button below to record audio.")
+# Audio recording button. Have to set key manually, so widgets do not 
+# conflict with eachother.
+    audio_bytes = audio_recorder(
+            key="123", 
+            icon_name="square",
+            recording_color="#e8b62c",
+            neutral_color="#6aa36f",
+            text=""
+    )
+       
 
 # Check if the session state has the 'processed' attribute
-if not hasattr(st.session_state, 'processed'):
-    st.session_state.processed = False
+    if not hasattr(st.session_state, 'processed'):
+        st.session_state.processed = False
 
 
 # Only process if the 'processed' flag is not set
-if audio_bytes and not st.session_state.processed:
-    
-    # Writing audio bytes to the file
-    with open("TTS.wav", mode="wb") as f:
-        f.write(audio_bytes)
+    if audio_bytes and not st.session_state.processed:
+        
+        # Writing audio bytes to the file
+        with open("TTS.wav", mode="wb") as f:
+            f.write(audio_bytes)
 
-    # Reading from the file and translating
-    with open("TTS.wav", mode="rb") as f:  # Notice "rb" mode for reading bytes
-        response = openai.Audio.translate("whisper-1", f)["text"]
-    
-    st.write(response)
-    # Creating audio byte string.
+        # Reading from the file and translating
+        with open("TTS.wav", mode="rb") as f:  # Notice "rb" mode for reading bytes
+            response = openai.Audio.translate("whisper-1", f)["text"]
+        
+        st.write(response)
+        # Creating audio byte string.
 
-    # Generate GPP response:
+        # Generate GPP response:
 
-    chatbot_response = get_chat_gpt_response(response)
+        chatbot_response = get_chat_gpt_response(response)
 
-    audio = generate_eleven_labs_audio(chatbot_response, my_voice)
-    
-    # Custom auto play function, made for streamlit.   
-    autoplay_audio_from_bytes(audio)
-   
-    # Convert audio bytes into .wav.
-    with open('STT.wav', mode='wb') as f:
-        f.write(audio)
+        audio = generate_eleven_labs_audio(chatbot_response, my_voice)
+        
+        # Custom auto play function, made for streamlit.   
+        autoplay_audio_from_bytes(audio)
+       
+        # Convert audio bytes into .wav.
+        with open('STT.wav', mode='wb') as f:
+            f.write(audio)
 
-    # Sleep time is used, so that st does not re run until audio is 
-    # finished playing.
-    sleep_time = get_audio_duration("STT.wav")
+        # Sleep time is used, so that st does not re run until audio is 
+        # finished playing.
+        sleep_time = get_audio_duration("STT.wav")
 
-    time.sleep(sleep_time)
+        time.sleep(sleep_time)
 
-    # Set the 'processed' flag
-    st.session_state.processed = True
+        # Set the 'processed' flag
+        st.session_state.processed = True
 
-    # Rerun the app
-    st.experimental_rerun()
+        # Rerun the app
+        st.experimental_rerun()
 
 
 # Reset audio_bytes and processed flag for the next interaction
-audio_bytes = None
-st.session_state.processed = False
+    audio_bytes = None
+    st.session_state.processed = False
 
-   
+       
