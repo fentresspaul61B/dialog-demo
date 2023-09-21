a5724cbcfeecc0b7e1e916e635cc6a7668782fd3
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2023-09-21 14:31:52 -0700

    trying with sample rate, just to see if response happens.

--- UI.py
+++ UI.py
@@ -92,8 +92,8 @@ def main():
         # st.write("My type is:", type(audio_bytes))
 
         response = make_ser_prediction(base64.b64encode(audio_bytes).decode('utf-8'))
-        st.write(response)
-        st.write(response.text)
+        # st.write(response)
+        # st.write(response.text)
         return
         # Check if the session state has the 'processed' attribute
         if not hasattr(st.session_state, 'processed'):
